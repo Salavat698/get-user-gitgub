5303916a29c734534489870b357342d7b0bdd1a6
Salavat <salavatmerhatimovic@gmail.com>
2023-02-28 02:45:33 +0300

    sss

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "Salavat698",
+  "avatar": "https://avatars.githubusercontent.com/u/62602960?v=4",
+  "name": "Salavat Gallyamov",
+  "company": "Академия Яндекс",
+  "blog": "http://slt116.nomoredomains.monster/",
+  "location": "Набережные Челны, Татарстан, Россия.",
+  "bio": "Front-end developer;\r\nTelegram: @sltdark;\r\nПочта: salavatmerhatimovic@gmail.com;\r\n",
+  "twitter": "",
+  "repos": 60,
+  "followers": 2,
+  "following": 2,
+  "created": "2018-02-07T16:18:45Z",
+}
